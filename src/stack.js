a5cdf7ad58bfb23d5cbcdd8685ade3cacc052bbc
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-07 12:31:08 +0400

    feat: solve Stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
+  constructor() {
+    this.array = [];
+  }
+  push(value) {
     // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.array.push(value);
   }
 
   pop() {
     // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.array.pop();
   }
 
   peek() {
     // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.array[this.array.length - 1];
   }
 }
 
